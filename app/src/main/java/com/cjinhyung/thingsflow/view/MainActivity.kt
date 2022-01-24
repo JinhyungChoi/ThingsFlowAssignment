d65aeb9000d924c3c6d35d7a62c5e0612f4fae2b
jinhyungchoi <wlsgudchoi@gmail.com>
2022-01-24 19:54:21 +0900

    feat: modify MainActivity

--- app/src/main/java/com/cjinhyung/thingsflow/view/MainActivity.kt
+++ app/src/main/java/com/cjinhyung/thingsflow/view/MainActivity.kt
@@ -2,10 +2,12 @@ package com.cjinhyung.thingsflow.view
 
 import android.content.Intent
 import android.net.Uri
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.Observer
 import com.cjinhyung.thingsflow.R
@@ -16,17 +18,40 @@ import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
-    private val mainViewModel : MainViewModel by viewModels()
+    private val mainViewModel: MainViewModel by viewModels()
     private lateinit var adapter: MainAdapter
-    private lateinit var binding : ActivityMainBinding
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var org: String
+    private lateinit var repo: String
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
         setAdapter()
-        loadIssue()
         setObserver()
+        setListener()
     }
+
+    private fun setListener() {
+        binding.tvSearch.setOnClickListener {
+            val intent = Intent(applicationContext, PopupActivity::class.java)
+            popupCallback.launch(intent)
+        }
+    }
+
+    private val popupCallback =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+            if (it.resultCode == RESULT_OK) {
+                val data = it.data!!
+                binding.tvSearch.text =
+                    data.getStringExtra("org") + "/" + data.getStringExtra("repo")
+                org = data.getStringExtra("org")!!
+                repo = data.getStringExtra("repo")!!
+                loadIssue(org, repo)
+            } else {
+                Toast.makeText(applicationContext, "없는 레포 입니다.", Toast.LENGTH_SHORT).show()
+            }
+        }
 
     private fun setObserver() {
         mainViewModel.issues.observe(this, Observer {
@@ -35,24 +60,24 @@ class MainActivity : AppCompatActivity() {
         })
     }
 
-    private fun loadIssue() {
-        mainViewModel.loadGitIssue("google", "dagger")
+    private fun loadIssue(org: String, repo: String) {
+        mainViewModel.loadGitIssue(org, repo)
     }
 
     private fun setAdapter() {
         adapter = MainAdapter()
         binding.rvGitIssue.adapter = adapter
-        adapter.setOnItemClickListener(object: MainAdapter.OnItemClickListener{
+        adapter.setOnItemClickListener(object : MainAdapter.OnItemClickListener {
             override fun onItemClick(view: View, position: Int) {
-                if (position == 5){
+                if (position == 5) {
                     val intent = Intent()
                     intent.action = Intent.ACTION_VIEW
                     intent.setData(Uri.parse("https://thingsflow.com/ko/home"))
                     startActivity(intent)
                 } else {
-                    val intent = Intent(applicationContext, DetailActivity::class.java).apply{
-                        putExtra("org", "google")
-                        putExtra("repo", "dagger")
+                    val intent = Intent(applicationContext, DetailActivity::class.java).apply {
+                        putExtra("org", org)
+                        putExtra("repo", repo)
                         putExtra("issueNum", adapter.issues[position].number)
                     }
                     startActivity(intent)
